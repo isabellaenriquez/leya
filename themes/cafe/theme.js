def3e46ef828716c2697cb2c92ee216a08f7f3aa
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2021-01-16 18:29:55 -0500

    Added weather + quotes

--- themes/cafe/theme.js
+++ themes/cafe/theme.js
@@ -1,18 +1,77 @@
-function getTopTest(data) {
-    var top = document.getElementById("mostVisited");
-    var ul = top.appendChild(document.createElement("ul"));
+import { key } from '../key.js'
 
-    for (var i = 0; i < 3; i++) {
-        var li = ul.appendChild(document.createElement("li"));
-        var a = li.appendChild(document.createElement ("a"));
-        a.innerHTML = data[i].title;
-        a.href = data[i].url;
+document.addEventListener('DOMContentLoaded', () => {
+
+    // most visited
+    const top = document.getElementById("most-visited");
+    function getRecent(data) {
+        for (let i = 0; i < 3; i++) {
+            let p = document.createElement('p');
+            let a = document.createElement('a');
+            a.innerHTML = data[i].title;
+            a.href = data[i].url;
+            p.appendChild(a);
+            top.appendChild(p);
+        }
     }
-}
+    chrome.topSites.get(getRecent);
+    
+    // quote of the day
+    function getQuote(){
+        const quote = document.getElementById('quote');
+        fetch('https://api.quotable.io/random?maxLength=50')
+        .then(response => response.json())
+        .then(data => {
+            //console.log(`${data.content} —${data.author}`);
+            quote.innerHTML = `${data.content} —${data.author}`;
+        });
+    }
+    getQuote();
 
-window.onload = function() {
-    chrome.topSites.get(getTopTest);
-}
+    // weather
+    function getWeather(postalCode){
+        const weatherText = document.getElementById('weather');
+        fetch("https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + postalCode, {
+            "method": "GET"
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.location);
+            const location = document.getElementById('location');
+            location.innerHTML = data.location.name;
+            const weatherIcon = document.getElementById('weather-icon');
+            weatherIcon.src = "https:" + data.current.condition.icon;
+            const weather = document.getElementById('weather');
+            weather.innerHTML = data.current.condition.text;
+        });
+    }
+    //getWeather();
+
+    const postalBtn = document.getElementById('postal-btn');
+    postalBtn.addEventListener('click', () =>{
+        const postalCode = document.getElementById('postal-input').value;
+        getWeather(postalCode);
+    })
+
+    // see/close menu
+    const menuBtn = document.getElementById('menu-btn');
+    menuBtn.addEventListener('click', () => {
+        const menus = document.getElementsByClassName('menu');
+        console.log(menus);
+        for (let menu of menus){
+            if (menu.style.display == 'none') {
+                menu.style.display = 'block';
+                console.log('display menu');
+                menuBtn.innerHTML = 'Close menu';
+            } else {
+                menu.style.display = 'none';
+                console.log('close menu');
+                menuBtn.innerHTML = 'See menu'
+            }
+        }
+    });
+    
+});
 
 
 
