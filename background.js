763108258f3fcfa6c4fa3ab2e6211fee6cb65874
yebrandon <contactbrandonye@gmail.com>
2021-01-16 18:23:34 -0500

    remove node modules and audio files

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var pageConditions = {
+const pageConditions = {
 	conditions: [
 		new chrome.declarativeContent.PageStateMatcher({
 			pageUrl: { schemes: ['https', 'http'] }
@@ -7,8 +7,43 @@ var pageConditions = {
 	actions: [new chrome.declarativeContent.ShowPageAction()]
 };
 
+const cafeAudio = new Audio(chrome.runtime.getURL('audio/cafe.mp3'));
+const rainAudio = new Audio(chrome.runtime.getURL('audio/rain.mp3'));
+const beachAudio = new Audio(chrome.runtime.getURL('audio/beach.mp3'));
+
+const audios = [
+	{ name: 'cafe', file: cafeAudio },
+	{ name: 'rain', file: rainAudio },
+	{ name: 'beach', file: beachAudio }
+];
+
 chrome.runtime.onInstalled.addListener(function () {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 		chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
 	});
+});
+
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+	function muteElement(elem) {
+		elem.muted = true;
+		elem.pause();
+	}
+
+	function muteAll() {
+		document.querySelectorAll('audio').forEach((elem) => muteElement(elem));
+	}
+
+	// Handle playing/pausing audio files
+	audios.forEach((audio) => {
+		if (audio.name == request.action) {
+			audio.file.play();
+		} else {
+			audio.file.pause();
+		}
+	});
+
+	//Handle mute
+	if (request.action == 'muteAll') {
+		muteAll();
+	}
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sound = document.getElementById('soundSwitch');
-    let soundLabel = document.getElementById('soundLabel');
+	const radios = document.getElementsByName('flexRadioDefault');
+	const sound = document.getElementById('soundSwitch');
+	let soundLabel = document.getElementById('soundLabel');
 
-    sound.addEventListener('change', () => {
-        if (sound.checked) {
-            soundLabel.innerHTML = 'sound on';
-            //console.log('on');
-        } else if (!sound.checked) {
-            soundLabel.innerHTML = 'sound off';
-            //console.log('off');
-        }
-    });
+	// Send messages for radio buttons
+	radios.forEach((radio) => {
+		radio.addEventListener('change', (e) => {
+			console.log(e);
+			chrome.runtime.sendMessage({ action: e.target.id });
+		});
+	});
+
+	// Send messages for sound toggle
+	sound.addEventListener('change', () => {
+		if (sound.checked) {
+			soundLabel.innerHTML = 'sound on';
+			chrome.runtime.sendMessage({ action: 'play' });
+		} else if (!sound.checked) {
+			soundLabel.innerHTML = 'sound off';
+			chrome.runtime.sendMessage({ action: 'mute' });
+		}
+	});
 });
-
