2944d255e000cf06b02d8b4b6cdca5efe6b9bc79
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2021-01-17 00:50:40 -0500

    tailored button messages to themes + last theme memory + aesthetics

--- themes/newTab.js
+++ themes/newTab.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
+    const themeFile = document.getElementById('themeFile');
+
+    chrome.storage.local.get(['selection'], function (result) {
+        console.log(result.selection);
+        let themeChoice = result.selection;
+		if (themeChoice == 'none') {
+			themeFile.data = 'cafe/cafe.html';
+		} else {
+			themeFile.data = themeChoice + '/' + themeChoice + '.html';
+		}
+	});
+
     // listen for change in radio
     chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
         const themes = ['cafe', 'rain', 'beach'];
@@ -6,10 +18,10 @@ document.addEventListener('DOMContentLoaded', () =>{
         console.log('received message: ' + request.selection);
 
         themes.forEach((theme) => {
-            if (theme == request.selection){
-                const themeFile = document.getElementById('themeFile');
+            if (theme == request.selection) {
                 themeFile.data = theme + '/' + theme + '.html';
             }
         });
     });
+
 })
