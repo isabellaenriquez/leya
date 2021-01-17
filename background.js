c0b01d2896aa8a2e088f23ea54b152e544635877
yebrandon <contactbrandonye@gmail.com>
2021-01-17 00:36:29 -0500

    ui saves

--- background.js
+++ background.js
@@ -10,17 +10,17 @@ const pageConditions = {
 const cafeAudio = new Audio(chrome.runtime.getURL('audio/cafe.mp3'));
 const rainAudio = new Audio(chrome.runtime.getURL('audio/rain.mp3'));
 const beachAudio = new Audio(chrome.runtime.getURL('audio/beach.mp3'));
-
 const audios = [
 	{ name: 'cafe', file: cafeAudio },
 	{ name: 'rain', file: rainAudio },
 	{ name: 'beach', file: beachAudio }
 ];
-
 const audioNames = audios.map((audio) => audio.name);
+
 let muted = false;
 
 chrome.runtime.onInstalled.addListener(function () {
+	chrome.storage.local.set({ selection: 'none', muted: false });
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 		chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
 	});
@@ -29,6 +29,7 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
 	// Handle playing/pausing audio files
 	function playAudio(name) {
+		console.log(name);
 		audios.forEach((audio) => {
 			if (audio.name == name && !muted) {
 				audio.file.play();
@@ -41,14 +42,11 @@ chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
 	//Handle requests
 	if (request.action == 'play') {
 		playAudio(request.selection);
-	}
-	if (request.action == 'mute') {
+	} else if (request.action == 'mute') {
 		muted = true;
 		playAudio(request.selection);
 	} else if (request.action == 'unmute') {
 		muted = false;
-		playAudio(request.selection);
-	} else if (audioNames.includes(request.selection)) {
-		playAudio(request.selection);
+		playAudio(request.selection.selection); // TODO: look at this?
 	}
 });
--- popup/popup.js
+++ popup/popup.js
@@ -3,19 +3,46 @@ document.addEventListener('DOMContentLoaded', () => {
 	const sound = document.getElementById('soundSwitch');
 	let soundLabel = document.getElementById('soundLabel');
 
-	// Send messages for radio buttons
+	// Set UI from storage
+
+	chrome.storage.local.get(['selection'], function (result) {
+		console.log(result.selection);
+		if (result.selection == 'none') {
+			radios.forEach((radio) => {
+				radio.checked = false;
+			});
+		} else {
+			document.getElementById(result.selection).checked = true;
+		}
+	});
+
+	chrome.storage.local.get(['muted'], function (result) {
+		console.log(result.muted);
+		if (result.muted) {
+			soundLabel.innerHTML = 'sound off';
+			sound.checked = false;
+		} else {
+			soundLabel.innerHTML = 'sound on';
+			sound.checked = true;
+		}
+	});
+
+	// Send messages for selecting radio buttons
 	radios.forEach((radio) => {
 		radio.addEventListener('change', (e) => {
-			chrome.runtime.sendMessage({
-				action: 'play',
-				selection: e.target.id
+			chrome.storage.local.set({ selection: e.target.id }, function () {
+				chrome.runtime.sendMessage({
+					action: 'play',
+					selection: e.target.id
+				});
 			});
 		});
 	});
 
 	// Send messages for sound toggle
 	sound.addEventListener('change', () => {
-		const selectedRadio = () => {
+		// Returns promise the resolves to selected radio button
+		const getSelectedRadio = () => {
 			return new Promise((resolve) => {
 				radios.forEach((radio) => {
 					if (document.getElementById(radio.id).checked) {
@@ -26,22 +53,43 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		};
 
+		// Handle sound toggle messages
 		if (sound.checked) {
-			selectedRadio().then((response) => {
+			chrome.storage.local.get(['selection'], function (response) {
+				chrome.storage.local.set({ muted: false }, function () {
+					soundLabel.innerHTML = 'sound on';
+					chrome.runtime.sendMessage({
+						action: 'unmute',
+						selection: response
+					});
+				});
+			});
+
+			/* 			getSelectedRadio().then((response) => {
 				soundLabel.innerHTML = 'sound on';
 				chrome.runtime.sendMessage({
 					action: 'unmute',
 					selection: response
 				});
-			});
+			}); */
 		} else if (!sound.checked) {
-			selectedRadio().then((response) => {
+			chrome.storage.local.get(['selection'], function (response) {
+				chrome.storage.local.set({ muted: true }, function () {
+					soundLabel.innerHTML = 'sound off';
+					chrome.runtime.sendMessage({
+						action: 'mute',
+						selection: response
+					});
+				});
+			});
+
+			/* 			getSelectedRadio().then((response) => {
 				soundLabel.innerHTML = 'sound off';
 				chrome.runtime.sendMessage({
 					action: 'mute',
 					selection: response
 				});
-			});
+			}); */
 		}
 	});
 });
